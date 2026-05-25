ee5d98ac98b0298420be8caedd1fb4e234b56553
debabrata4934-hue <debabrata4934@gmail.com>
2026-05-25 15:54:34 +0530

    Add home page content sections

--- src/landing_page/home/Award.jsx
+++ src/landing_page/home/Award.jsx
@@ -1,10 +1,49 @@
 
 function Award() {
     return ( 
-        <h1>
-            Award
-        </h1>
-     );
+        <div className="container mb-5">
+            <div className="row">
+                <div className="col-6">
+                    <img src="\media\images\largestBroker.svg" alt="Award image"  />
+                </div>
+                <div className="col-6">
+                    <h1 className="mb-3">Largest stock broker in India</h1>
+                    <p>2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                    <div className="row mt-5 mb-5">
+                        <div className="col-6">
+                            <ul>
+                                <li>
+                                    <p>Futures and Options</p>
+                                </li>
+                                <li>
+                                    <p>Commodity derivatives</p>
+                                </li>
+                                <li>
+                                    <p>Currency derivatives</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-6">
+                            <ul>
+                                <li>
+                                    <p>Stocks & IPOs</p>
+                                </li>
+                                <li>
+                                    <p>Direct mutual funds</p>
+                                </li>
+                                <li>
+                                    <p>Bonds and Govt. Securities</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <img src="\media\images\pressLogos.png" alt="Award image" style={{width: "90%"}} />
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Award;
--- src/landing_page/home/Stats.jsx
+++ src/landing_page/home/Stats.jsx
@@ -1,7 +1,18 @@
 function Stats() {
     return ( 
-        <h1>Stats</h1>
-     );
+        <div className="container">
+            <div className="row">
+                <div className="col-6"></div>
+                <div className="col-6">
+                    <img src="\media\images\ecosystem.png" alt="Stats image" />
+                    <div>
+                        <a href="/">Explore our products</a>
+                        <a href="/">Try Kite demo</a>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    );
 }
 
 export default Stats;
